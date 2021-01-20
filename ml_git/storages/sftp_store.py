2e878eedba0739e003de57cb796bc1c83bfd0198
Felipe Muniz <felipemuniznet@gmail.com>
2021-01-20 10:25:11 -0300

    Suport for private key password.

--- ml_git/storages/sftp_store.py
+++ ml_git/storages/sftp_store.py
@@ -29,7 +29,16 @@ class SFtpStore(Store):
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        user_private_key = paramiko.RSAKey.from_private_key_file(self._private_key)
+        #user_private_key = paramiko.RSAKey.from_private_key_file(self._private_key)
+
+        user_private_key = None
+
+        try:
+            user_private_key = paramiko.RSAKey.from_private_key_file(self._private_key)
+        except:
+            private_key_password = input("Enter passprhrase for key '" + self._private_key + "': ") 
+            user_private_key = paramiko.RSAKey.from_private_key_file(self._private_key, password=private_key_password)
+
         ssh_client.connect(self._host, port=self._port, username=self._username, pkey=user_private_key)
 
         open_session = ssh_client.get_transport().open_session()
