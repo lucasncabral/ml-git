5a3230129223c3332c3020a23739516d592468e5
Lucas <lucas.cabral@ccc.ufcg.edu.br>
2022-07-12 11:57:22 -0300

    Removing unnecessary test

--- tests/integration/test_41_wizard_commands.py
+++ tests/integration/test_41_wizard_commands.py
@@ -12,8 +12,8 @@ import pytest
 from ml_git.commands.wizard import WizardMode
 from ml_git.ml_git_message import output_messages
 from tests.integration.commands import MLGIT_REMOTE_FSCK, MLGIT_CHECKOUT, MLGIT_COMMIT, MLGIT_CONFIG_WIZARD, \
-    MLGIT_CREATE, MLGIT_INIT, MLGIT_ENTITY_INIT, MLGIT_REMOTE_ADD
-from tests.integration.helper import MLGIT_ADD, GLOBAL_CONFIG_PATH, DATASETS, MODELS, GIT_PATH, ERROR_MESSAGE
+    MLGIT_CREATE, MLGIT_INIT
+from tests.integration.helper import MLGIT_ADD, GLOBAL_CONFIG_PATH, DATASETS, MODELS
 from tests.integration.helper import check_output
 
 
@@ -68,23 +68,4 @@ class WizardCommandsAcceptanceTests(unittest.TestCase):
                       check_output(MLGIT_ADD % ('models', 'ENTITY-NAME', '--wizard')))
 
         self.assertIn(output_messages['ERROR_NOT_INITIALIZED'] % MODELS,
-                      check_output(MLGIT_COMMIT % ('models', 'ENTITY-NAME', '--wizard')))
-
-    @pytest.mark.usefixtures('start_local_git_server', 'switch_to_tmp_dir')
-    @mock.patch.dict(os.environ, {'HOME': GLOBAL_CONFIG_PATH})
-    def test_04_wizard_commands_entity_not_exists(self):
-        self.setup_wizard()
-        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_INIT))
-        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_REMOTE_ADD % (DATASETS, os.path.join(self.tmp_dir, GIT_PATH))))
-        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_REMOTE_ADD % (MODELS, os.path.join(self.tmp_dir, GIT_PATH))))
-        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_ENTITY_INIT % DATASETS))
-        self.assertNotIn(ERROR_MESSAGE, check_output(MLGIT_ENTITY_INIT % MODELS))
-
-        self.assertIn(output_messages['ERROR_WRONG_NAME'],
-                      check_output(MLGIT_ADD % ('datasets', 'ENTITY-NAME', '--wizard')))
-
-        self.assertIn(output_messages['ERROR_WRONG_NAME'],
-                      check_output(MLGIT_REMOTE_FSCK % ('datasets', 'ENTITY-NAME') + ' --wizard'))
-
-        self.assertIn(output_messages['ERROR_WRONG_NAME'],
                       check_output(MLGIT_COMMIT % ('models', 'ENTITY-NAME', '--wizard')))
